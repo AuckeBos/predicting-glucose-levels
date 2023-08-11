ea888da6c111deb6e7b335d28d33de9805a64696
Aucke Bos <aucke.bos97@gmail.com>
2023-08-11 18:40:14 +0200

    Wip: deployment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
-nohup prefect server start &
+# nohup prefect server start &
 # Await server start for 10 seconds
-sleep 5
-prefect work-pool create --type process default-pool
-prefect worker start -p default-pool
+# sleep 10
+# prefect work-pool create --type process default-pool
+# prefect worker start -p default-pool
+tail -f /dev/null
